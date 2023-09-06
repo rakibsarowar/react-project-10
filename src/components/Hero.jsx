4f83ec7db6de7e849f9d1ec3a128c40566c238e4
rakibsarowar <rakibsarowar@gmail.com>
2023-09-06 22:57:57 +0600

    working on hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+    return (
+        <div>
+
+          <h1 className="text-6xl lg:text-9xl md:text-7xl font-semibold tracking-tighter text-gray-700 mb-11 md:mb-14 pb-4 md:pb-4 bg-gradient-to-br from-orange-500 to-amber-200 text-transparent bg-clip-text" data-sal-delay="200" data-sal-easing="ease" data-sal-duration="800">
+            
+            Rakib,<br/>
+            Experience<br/>
+            Designer
+          
+          </h1>
+
+        </div>
+    );
+};
+
+export default Hero;
+
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import Hero from "../Hero";
 
 const Main = () => {
     return (
-        <div>
-            <Header></Header>
+        <div className="bg-gray-900">
+            <Hero></Hero>
             <Outlet></Outlet>
         </div>
     );
