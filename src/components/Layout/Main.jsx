9f2de2bc77a276be443655f69b8866cb06c27eb3
rakibsarowar <rakibsarowar@gmail.com>
2023-09-09 23:20:40 +0600

    working on related css

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -4,8 +4,8 @@ import Hero from "../Hero";
 const Main = () => {
     return (
         <div className="bg-gray-900 ">
-            <div className="container mx-auto px-5 md:pt-0 relative">
-                <img className="mb-20 hidden xl:block absolute" src="/src/assets/element.svg" alt="" />
+            <div className="container mx-auto  px-5 md:pt-0 relative">
+                <img className="mt-5 hidden xl:block absolute" src="/src/assets/element.svg" alt="" />
             </div>
             <Hero></Hero>
             <Outlet></Outlet>
