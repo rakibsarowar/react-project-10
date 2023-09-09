eca565972c2bf05f96e603814584831eb21f0159
rakibsarowar <rakibsarowar@gmail.com>
2023-09-09 23:04:49 +0600

    expreminting design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,24 @@
 const Hero = () => {
-    return (
-        <div>
+  return (
+    <div className="h-screen w-10/12 px-14 mx-auto max-w-screen-xl pt-24">
+      <h1 className="text-6xl lg:text-9xl md:text-7xl font-semibold tracking-tighter text-gray-700 mb-11 md:mb-14 pb-4 md:pb-4 bg-gradient-to-br from-orange-500 to-amber-200 text-transparent bg-clip-text text-left">
 
-          <h1 className="text-6xl lg:text-9xl md:text-7xl font-semibold tracking-tighter text-gray-700 mb-11 md:mb-14 pb-4 md:pb-4 bg-gradient-to-br from-orange-500 to-amber-200 text-transparent bg-clip-text" data-sal-delay="200" data-sal-easing="ease" data-sal-duration="800">
-            
-            Rakib,<br/>
-            Experience<br/>
-            Designer
-          
-          </h1>
-
-        </div>
-    );
+        Rakib,<br />
+        Experience<br />
+        Developer
+      </h1>
+      <div className="text-xl md:text-2xl text-gray-400">
+        <p className="mb-10 leading-relaxed font-normal subpixel-antialiased">
+          Real passion for human-centered design solutions, also have extensive experience as Front End developer. Read my journal or my projects if you want to know more about me.
+          <a direction="up" className="ease-in font-normal underline underline-offset-8 decoration-2 decoration-amber-400 text-white hover:text-white hover:decoration-white" href="/journal/">journal</a>
+        </p>
+        <p className="mb-10 leading-relaxed font-normal subpixel-antialiased">
+          Real passion for human-centered design solutions, also have extensive experience as Front End developer. Read my journal or my projects if you want to know more about me.
+          <a direction="up" className="ease-in font-normal underline underline-offset-8 decoration-2 decoration-amber-400 text-white hover:text-white hover:decoration-white" href="/journal/">journal</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header";
 import Hero from "../Hero";
 
 const Main = () => {
     return (
-        <div className="bg-gray-900">
+        <div className="bg-gray-900 ">
             <Hero></Hero>
             <Outlet></Outlet>
         </div>
